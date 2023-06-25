31d6b6763ba6313a91b3b8ce713fa4144c39b940
Aakash Shah <subs@appsparkler.com>
2023-06-25 20:49:24 +0530

    test: move sample data to separate file

---
+++ app/src/main/java/com/example/hfncheckins/data/sample/getSampleEvent.kt
@@ -0,0 +1,12 @@
+package com.example.hfncheckins.data.sample
+
+import com.example.hfncheckins.model.Event
+import java.util.*
+
+fun getSampleEvent() = Event(
+    description = "Chariji's Birth Anniversary",
+    title = "Chariji Maharaj's 95th Birth Anniversary Celebration",
+    endDate = Date(),
+    startDate = Date(),
+    id = "1234"
+)

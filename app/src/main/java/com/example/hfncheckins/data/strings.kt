deb9e1dcc398c0d668f856693636ec4f3dd92228
Aakash Shah <subs@appsparkler.com>
2023-06-25 20:49:43 +0530

    test: create strings object various static strings in the app

---
+++ app/src/main/java/com/example/hfncheckins/data/strings.kt
@@ -0,0 +1,7 @@
+package com.example.hfncheckins.data
+
+object strings {
+    val inputInstructions = "Enter your abhyasi id, mobile # or email address"
+    val startCheckin = "Start Checkin"
+    val info = "Please enter info..."
+}

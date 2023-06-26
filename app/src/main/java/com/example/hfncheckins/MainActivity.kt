eb680bbe9ddbafdc4d1add0d2e0d7028979cd9ec
Aakash Shah <subs@appsparkler.com>
2023-06-26 10:27:55 +0530

    fix: remove input-screen reference from MainActivity

--- app/src/main/java/com/example/hfncheckins/MainActivity.kt
+++ app/src/main/java/com/example/hfncheckins/MainActivity.kt
@@ -6,17 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hfncheckins.data.sample.getSampleEvent
 import com.example.hfncheckins.hfnTheme.HFNTheme
-import com.example.hfncheckins.model.Event
-import com.example.hfncheckins.ui.components.InputScreen.InputScreen
-import com.example.hfncheckins.ui.theme.HFNCheckinsTheme
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
@@ -37,13 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HFNTheme {
                 Scaffold {
-                    InputScreen(
-                        modifier = Modifier.padding(it),
-                        scanner = scanner,
-                        event = getSampleEvent(),
-                        onValidStart = {},
-                        onValidScan = {}
-                    )
+
                 }
             }
         }

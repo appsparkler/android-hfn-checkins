cdeaf9481ecf8bd08f3be7a2c9a31f91c8583fff
Aakash Shah <subs@appsparkler.com>
2023-06-27 15:56:27 +0530

    chore: render app in MainActivity

--- app/src/main/java/com/example/hfncheckins/MainActivity.kt
+++ app/src/main/java/com/example/hfncheckins/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.hfncheckins
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.example.hfncheckins.hfnTheme.HFNTheme
+import com.example.hfncheckins.ui.components.App
 import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import java.util.*
@@ -31,36 +25,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             HFNTheme {
                 Scaffold {
-
+                        App()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun App(
-    scanner: GmsBarcodeScanner
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            scanner.startScan()
-                .addOnSuccessListener { barcode ->
-                    // Task completed successfully
-                }
-                .addOnCanceledListener {
-                    // Task canceled
-                }
-                .addOnFailureListener { e ->
-                    // Task failed with an exception
-                }
-        }) {
-            Text(text = "SCAN")
         }
     }
 }

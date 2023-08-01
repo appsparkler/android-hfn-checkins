3dc9ac3789890e09f94432b4974aa78e3f094e05
Aakash Shah <appsparkler@gmail.com>
2023-08-02 04:20:19 +0530

    chore: minor clean up

--- app/src/main/java/com/example/hfncheckins/ui/components/AbhyasiIdCheckinScreen/AbhyasiIdCheckinScreen.kt
+++ app/src/main/java/com/example/hfncheckins/ui/components/AbhyasiIdCheckinScreen/AbhyasiIdCheckinScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hfncheckins.ui.theme.HFNCheckinsTheme
-import com.example.hfncheckins.viewModel.AbhyasiIdCheckin
 
 @Composable
 fun AbhyasiIdCheckinScreen(

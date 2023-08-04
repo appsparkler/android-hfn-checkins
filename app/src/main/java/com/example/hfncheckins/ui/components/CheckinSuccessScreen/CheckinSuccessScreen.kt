3c64ec03ceddbdf8bb04d7ea9b79697aa8d84f16
Aakash Shah <appsparkler@gmail.com>
2023-08-04 10:40:05 +0530

    chore: add konfetti :)

--- app/src/main/java/com/example/hfncheckins/ui/components/CheckinSuccessScreen/CheckinSuccessScreen.kt
+++ app/src/main/java/com/example/hfncheckins/ui/components/CheckinSuccessScreen/CheckinSuccessScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,16 +19,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hfncheckins.R
 import com.example.hfncheckins.ui.theme.HFNCheckinsTheme
+import nl.dionsegijn.konfetti.compose.KonfettiView
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
 
 @Composable
 fun CheckinSuccessScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClickReturnToMain: () -> Unit
 ) {
+    val party = Party(
+        speed = 0f,
+        maxSpeed = 30f,
+        damping = 0.9f,
+        spread = 360,
+        colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+        position = Position.Relative(0.5, 0.3),
+        emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100)
+    )
+    KonfettiView(
+        modifier = Modifier.fillMaxSize(),
+        parties = listOf(party),
+    )
     Column(
         modifier = modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.spacedBy(
+            12.dp,
+            Alignment.CenterVertically
+        )
     ) {
         Icon(
             painter = painterResource(
@@ -35,6 +60,11 @@ fun CheckinSuccessScreen(
             contentDescription = "Checkin success",
             tint = Color(0xFF4BB543)
         )
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text="RETURN TO MAIN SCREEN",
+            )
+        }
     }
 }
 
@@ -44,7 +74,10 @@ fun CheckinSuccessScreenPreview() {
     HFNCheckinsTheme {
         Scaffold {
             CheckinSuccessScreen(
-                modifier = Modifier.padding(it)
+                modifier = Modifier.padding(it),
+                onClickReturnToMain = {
+
+                }
             )
         }
     }

47174a7970b89d6175eef8f54858c0065c7d2b14
Aakash Shah <appsparkler@gmail.com>
2023-09-17 11:11:05 +0530

    chore: update abhyasi-id validation logic

--- app/src/test/java/com/example/hfncheckins/utils/isValidAbhyasiIdTest.kt
+++ app/src/test/java/com/example/hfncheckins/utils/isValidAbhyasiIdTest.kt
@@ -14,4 +14,15 @@ class isValidAbhyasiIdTest {
     fun test_invalidCase() {
         assertEquals(false, isValidAbhyasiId("Hello World"))
     }
+
+    @Test
+    fun test_newSeeker_lower_case() {
+        assertEquals(true, isValidAbhyasiId("b33938392"))
+    }
+
+    @Test
+    fun test_newSeeker_upper_case() {
+        assertEquals(true, isValidAbhyasiId("H33938392"))
+
+    }
 }

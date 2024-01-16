59ecafe25be450086135ad6aea8981de25298691
Aakash Shah <appsparkler@gmail.com>
2024-01-16 13:49:39 +0530

    chore: wip

--- app/src/main/java/com/appsparkler/hfncheckins/ui/components/MainScreen/FetchingEvents.kt
+++ app/src/main/java/com/appsparkler/hfncheckins/ui/components/MainScreen/FetchingEvents.kt
@@ -1,20 +1,14 @@
 package com.appsparkler.hfncheckins.ui.components.MainScreen
 
-import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.appsparkler.hfncheckins.ui.components.common.VerticalSpacer12Dp
 
 @Composable fun FetchingEvents() {
@@ -25,7 +19,7 @@ import com.appsparkler.hfncheckins.ui.components.common.VerticalSpacer12Dp
   ) {
     CircularProgressIndicator()
     VerticalSpacer12Dp()
-    Text(text = ("Fetching Events..."))
+    Text(text = ("Fetching Events"))
   }
 }
 

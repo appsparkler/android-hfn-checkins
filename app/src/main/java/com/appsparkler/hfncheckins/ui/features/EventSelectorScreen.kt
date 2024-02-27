daf543b128b7b7ce14af0d72b4e3037c26a60555
Aakash Shah <appsparkler@gmail.com>
2024-02-27 11:37:11 +0530

    chore: setup event selector screen

---
+++ app/src/main/java/com/appsparkler/hfncheckins/ui/features/EventSelectorScreen.kt
@@ -0,0 +1,41 @@
+package com.appsparkler.hfncheckins.ui.features
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable fun EventSelectorScreen(
+  modifier: Modifier = Modifier
+) {
+  Column(
+    modifier = modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+      Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        CircularProgressIndicator()
+        Text(text = "fetching ongoing events")
+      }
+  }
+}
+
+@Preview @Composable fun EventSelectorScrenPreview() {
+  Scaffold {
+    EventSelectorScreen(
+      modifier = Modifier.padding(it)
+    )
+  }
+}

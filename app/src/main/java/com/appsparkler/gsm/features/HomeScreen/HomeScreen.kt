03c982e5cf1613273e3223fd5958582a596ed7db
Aakash Shah <appsparkler@gmail.com>
2024-03-08 09:10:08 +0530

    chore: inclrease size of image

--- app/src/main/java/com/appsparkler/gsm/features/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/appsparkler/gsm/features/HomeScreen/HomeScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
@@ -13,6 +15,8 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,6 +46,8 @@ fun HomeScreen(
       verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
       Image(
+        modifier = Modifier.scale(1.5f)
+          .padding(12.dp),
         painter = painterResource(id = R.drawable.image),
         contentDescription = null
       )

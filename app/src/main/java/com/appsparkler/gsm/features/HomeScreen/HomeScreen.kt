5fc87020094fb7704edc542e8d22cef03026ae52
Aakash Shah <appsparkler@gmail.com>
2024-03-08 09:10:31 +0530

    chore: minor clean up

--- app/src/main/java/com/appsparkler/gsm/features/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/appsparkler/gsm/features/HomeScreen/HomeScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
@@ -16,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
